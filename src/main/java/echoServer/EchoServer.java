1cadf3edc510935a767c4a4b06fe31f9bda06390
JeongBongSeong <43163888+JeongBongSeong@users.noreply.github.com>
2020-01-31 10:51:59 +0900

    Update EchoServer.java

--- src/main/java/echoServer/EchoServer.java
+++ src/main/java/echoServer/EchoServer.java
@@ -32,15 +32,15 @@ public class EchoServer {
             brRecv = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
             pwSend = new PrintWriter(mSocket.getOutputStream());
             while (true) {
-                String strRecvMsg = brRecv.readLine();		//클라이언트 메시지 수신
+                String strRecvMsg = brRecv.readLine();		// 
                 System.out.println(strRecvMsg);
 
                 if (strRecvMsg.equals("exit")) {
                     break;
                 }
 
-                // 클라이언트에 문자열 전송
-                pwSend.println(strRecvMsg);		//메시지 echo
+                // 클
+                pwSend.println(strRecvMsg);		//
                 pwSend.flush();
             }
             mSocket.close();
@@ -53,41 +53,3 @@ public class EchoServer {
         new EchoServer().echoServer();
     }
 }
-/*
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class echoServer { 
-
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(11111);
-                Socket socket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(socket.getOutputStream());
-                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
- 
-            System.out.println("서버 대기중....");
-            while (true) {
-                String msg = br.readLine();
-                if (msg.equalsIgnoreCase("Quit"))
-                    break;
-                System.out.println("클라이언트에서 보냄:[ " + msg + " ]");
-                out.println(msg);
-                out.flush();
-            }
-            System.out.println("서버종료..");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-}
-*/
